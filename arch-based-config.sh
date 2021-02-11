116c8c106514f14f5bfbf823a34f7f1c39bceb92
Bruno Martins Correa <bruno.mcorrea@hotmail.com>
2021-02-11 17:42:24 -0300

    Adding arduino and perl

--- arch-based-config.sh
+++ arch-based-config.sh
@@ -6,7 +6,7 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru -S --needed --noconfirm zsh xclip tree vim curl net-tools openvpn zip unzip git jq htop python python-pip erlang elixir npm nvm go nasm ruby clisp ghc cabal-install stack r julia php lua vala vala-lint-git ninja meson heroku-cli docker-compose ctop postgresql minikube kubectl helm k9s kind flatpak chromium intellij-idea-community-edition pycharm-community-edition android-studio visual-studio-code-bin postman-bin visualvm oh-my-zsh-git zsh-syntax-highlighting powerline-fonts ttf-jetbrains-mono ttf-ms-fonts ttf-wps-fonts telegram-desktop slack-desktop steam dbeaver android-tools android-sdk android-ndk android-emulator flutter
+paru -S --needed --noconfirm zsh xclip tree vim curl net-tools openvpn zip unzip git jq htop python python-pip erlang elixir npm nvm go nasm ruby perl clisp ghc cabal-install stack r julia php lua vala vala-lint-git ninja meson arduino arduino-avr-core heroku-cli docker-compose ctop postgresql minikube kubectl helm k9s kind flatpak chromium intellij-idea-community-edition pycharm-community-edition android-studio visual-studio-code-bin postman-bin visualvm oh-my-zsh-git zsh-syntax-highlighting powerline-fonts ttf-jetbrains-mono ttf-ms-fonts ttf-wps-fonts telegram-desktop slack-desktop steam dbeaver android-tools android-sdk android-ndk android-emulator flutter
 
 # FLATPAK
 flatpak install -y flathub com.wps.Office com.discordapp.Discord com.spotify.Client
@@ -66,6 +66,10 @@ nvm install node
 # NPM
 sudo npm install -g yarn react-native-cli create-react-app create-next-app @nestjs/cli @vue/cli json-server expo-cli
 # NPM
+
+# ARDUINO
+sudo usermod -aG uucp $USER
+# ARDUINO
 
 # SDKMAN
 curl -s "https://get.sdkman.io" | zsh
