22892923561bb351ce970e715129b4e9ccd6d991
Bruno Martins Correa <bruno.mcorrea@hotmail.com>
2021-02-13 13:23:29 -0300

    Splitting update and installation

--- arch-based-dev-config.sh
+++ arch-based-dev-config.sh
@@ -7,7 +7,8 @@ function isInstalled() {
     fi
 }
 
-sudo pacman -Syyu --needed --noconfirm base-devel cmake zsh zsh-syntax-highlighting powerline-fonts ttf-jetbrains-mono xclip tree vim curl net-tools openvpn zip unzip git jq yq htop python python-pip erlang elixir rust go nasm ruby perl clisp ghc cabal-install stack php lua vala ninja meson arduino arduino-avr-core docker-compose ctop minikube kubectl helm k9s smali jadx android-tools visualvm intellij-idea-community-edition pycharm-community-edition dbeaver
+sudo pacman -Syyu --noconfirm
+sudo pacman -S --needed --noconfirm base-devel cmake zsh zsh-syntax-highlighting powerline-fonts ttf-jetbrains-mono xclip tree vim curl net-tools openvpn zip unzip git jq yq htop python python-pip erlang elixir rust go nasm ruby perl clisp ghc cabal-install stack php lua vala ninja meson arduino arduino-avr-core docker-compose ctop minikube kubectl helm k9s smali jadx android-tools visualvm intellij-idea-community-edition pycharm-community-edition dbeaver
 
 if [ $(isInstalled paru) == 0 ]; then
     cd /tmp
@@ -82,12 +83,6 @@ fi
 # ARDUINO
 sudo usermod -aG uucp $USER
 # ARDUINO
-
-if [ $(isInstalled zsh) == 1 ] && [ $(isInstalled sdk) == 0 ]; then
-    echo "Deu ruim"
-else
-    echo "Foi"
-fi
 
 # SDKMAN
 if [ $(isInstalled zsh) == 1 ] && [ $(isInstalled sdk) == 0 ]; then
