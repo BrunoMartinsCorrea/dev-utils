bf218f48490b9c92137ea1a61194642059b32f87
Bruno Martins Correa <bruno.mcorrea@hotmail.com>
2021-04-11 13:22:52 -0300

    Update vpn config script

--- vpn-config.sh
+++ vpn-config.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
-VPN_FILE=$(ls *.ovpn 2> /dev/null | head -1)
-if [ -n "$VPN_FILE" ]; then
-    echo "Importing $VPN_FILE...\n"
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NO_COLOR='\033[0m'
 
-    echo "Type your VPN username: "
-    read VPN_USERNAME
+mapfile -t VPN_FILES < <(find ~ -name "*.ovpn" 2>/dev/null)
 
-    echo "Type your VPN password: "
-    read -s VPN_PASSWORD
+if [ -n "$VPN_FILES" ]; then
+    echo -e "Choose a file to import...\n"
 
-    echo "Type your VPN alias: "
-    read VPN_ALIAS
+    select VPN_FILE in "${VPN_FILES[@]}";
+    do
+        test -z "$VPN_FILE" && break
+        echo -e "\n${YELLOW}Importing $VPN_FILE...${NO_COLOR}\n"
 
-    VPN_UUID=$(nmcli connection import type openvpn file ./$VPN_FILE | grep -Po "(?<=\()(\w+-?)+")
-    nmcli connection modify $VPN_UUID connection.id "$VPN_ALIAS"
-    nmcli connection modify $VPN_UUID ipv4.never-default true
-    nmcli connection modify $VPN_UUID ipv6.never-default true
-    nmcli connection modify $VPN_UUID +vpn.data username=$VPN_USERNAME
-    nmcli connection modify $VPN_UUID vpn.secrets "password=$VPN_PASSWORD"
+        read -p "Type your VPN username: " VPN_USERNAME
+        read -sp "Type your VPN password: " VPN_PASSWORD
+        echo
+        read -p "Type your VPN alias: " VPN_ALIAS
+
+        VPN_UUID=$(nmcli connection import type openvpn file "$VPN_FILE" 2>/dev/null | grep -Po "(\w+-)+\w+")
+
+        if [ -n "$VPN_UUID" ]; then
+            nmcli connection modify "$VPN_UUID" connection.id "$VPN_ALIAS"
+            nmcli connection modify "$VPN_UUID" +vpn.data username=$VPN_USERNAME
+            nmcli connection modify "$VPN_UUID" +vpn.secrets "password=$VPN_PASSWORD"
+            nmcli connection modify "$VPN_UUID" ipv4.never-default true
+            nmcli connection modify "$VPN_UUID" ipv6.never-default true
+
+            echo -e "\n${GREEN}VPN created!${NO_COLOR}"
+        else
+            echo -e "\n${RED}Oops! Something went wrong!${NO_COLOR}"
+            exit 1
+        fi
+        break
+    done
 else
-    echo "Ops! Where is the ovpn file?"
+    echo -e "${RED}Oops! I didn't find any .ovpn file...${NO_COLOR}"
+    exit 1
 fi
