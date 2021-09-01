7abf19b7135de519be800fba8147ef0c32e57de4
Bruno Martins Correa <bruno.correa@yssy.com.br>
2021-09-01 15:25:50 -0300

    Adds some packages

--- ubuntu-based-config.sh
+++ ubuntu-based-config.sh
@@ -3,8 +3,19 @@ cd /tmp/
 
 sudo apt update -y
 sudo apt full-upgrade -y
-sudo apt install -y cmake tree vim curl net-tools xclip zsh zsh-syntax-highlighting zsh-autosuggestions python3 python3-pip clang erlang elixir golang nasm ruby perl clisp haskell-platform lua5.3 valabind valac valac-bin valadoc ninja-build meson arduino kubernetes php apktool git ssh xclip zip unzip jq htop ctop qemu libvirt0 openvpn network-manager-openvpn-gnome openvpn-systemd-resolved fonts-powerline fonts-firacode fonts-noto-color-emoji docker-compose dconf-cli gnome-boxes gnome-tweak-tool gnome-sushi geary cheese gnome-audio gnome-builder gnome-calendar gnome-clocks gnome-contacts gnome-dictionary gnome-documents gnome-multi-writer gnome-photos gnome-remote-desktop gnome-shell-extension-gsconnect gnome-shell-extension-gsconnect-browsers gnome-sound-recorder gnome-system-log gnome-todo gnome-weather peek pitivi gnome-music
+sudo apt install -y cmake tree vim curl net-tools xclip neofetch zsh zsh-syntax-highlighting zsh-autosuggestions libaio-dev openjdk-11-jdk python3 python3-pip clang erlang elixir golang nasm ruby perl clisp haskell-platform lua5.3 valabind valac valac-bin valadoc ninja-build meson arduino kubernetes php apktool git ssh zip unzip jq xmlstarlet htop ctop qemu libvirt0 openvpn network-manager-openvpn-gnome openvpn-systemd-resolved fonts-powerline fonts-firacode fonts-noto-color-emoji docker-compose dconf-cli gnome-boxes gnome-tweak-tool gnome-sushi geary cheese gnome-audio gnome-builder gnome-calendar gnome-clocks gnome-contacts gnome-dictionary gnome-documents gnome-multi-writer gnome-photos gnome-remote-desktop gnome-shell-extension-gsconnect gnome-shell-extension-gsconnect-browsers gnome-sound-recorder gnome-system-log gnome-todo gnome-weather peek pitivi gnome-music
 sudo apt autoremove -y
+
+# ORACLE
+curl -LJ# \
+    -o instantclient-basic-linux.zip https://download.oracle.com/otn_software/linux/instantclient/213000/instantclient-basic-linux.x64-21.3.0.0.0.zip \
+    -o instantclient-sdk-linux.zip https://download.oracle.com/otn_software/linux/instantclient/213000/instantclient-sdk-linux.x64-21.3.0.0.0.zip
+
+unzip instantclient-basic-linux.zip
+unzip instantclient-sdk-linux.zip
+cd instantclient_*
+sudo cp *.so* /usr/lib
+# ORACLE
 
 # SNAP
 sudo snap refresh
