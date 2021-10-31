84690791a6c33db01dce5362228fa00de2cd7d58
Bruno Martins Correa <brunomsft@gmail.com>
2021-10-31 17:50:49 -0300

    Adds cpufetch

--- src/installer/core/any.sh
+++ src/installer/core/any.sh
@@ -4,10 +4,13 @@ packages_path=$dev_utils_path/installer/package
 
 source $packages_path/package-manager-plugin.sh
 source $packages_path/common-tools.sh
+source $packages_path/zsh.sh
 source $packages_path/flatpak.sh
 source $packages_path/docker.sh
-source $packages_path/docker-compose.sh
 source $packages_path/ctop.sh
+source $packages_path/vscode.sh
 source $packages_path/arduino.sh
 source $packages_path/rustup.sh
 source $packages_path/discord.sh
+source $packages_path/neofetch.sh
+source $packages_path/cpufetch.sh
