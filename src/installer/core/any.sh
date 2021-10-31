6e47577038e6f9735689193aedb5ff30922095de
Bruno Martins Correa <brunomsft@gmail.com>
2021-10-31 17:17:04 -0300

    Adds commom tools

--- src/installer/core/any.sh
+++ src/installer/core/any.sh
@@ -3,6 +3,7 @@
 packages_path=$dev_utils_path/installer/package
 
 source $packages_path/package-manager-plugin.sh
+source $packages_path/common-tools.sh
 source $packages_path/flatpak.sh
 source $packages_path/docker.sh
 source $packages_path/docker-compose.sh
