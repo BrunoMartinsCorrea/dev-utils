0c3330dfe6de7d66048686b7898e23cf9967cce2
Bruno Martins Correa <brunomsft@gmail.com>
2021-10-31 18:56:45 -0300

    Refactors how packages will be sourced

--- src/installer/core/any.sh
+++ src/installer/core/any.sh
@@ -2,20 +2,6 @@
 
 packages_path=$dev_utils_path/installer/package
 
-source $packages_path/package-manager-plugin.sh
-source $packages_path/common-tools.sh
-source $packages_path/zsh.sh
-source $packages_path/flatpak.sh
-source $packages_path/python.sh
-source $packages_path/docker.sh
-source $packages_path/kubernetes.sh
-source $packages_path/ctop.sh
-source $packages_path/vscode.sh
-source $packages_path/arduino.sh
-source $packages_path/rustup.sh
-source $packages_path/discord.sh
-source $packages_path/neofetch.sh
-source $packages_path/cpufetch.sh
-source $packages_path/jq.sh
-source $packages_path/yq.sh
-source $packages_path/htop.sh
+for package in $(ls $packages_path); do
+    source $packages_path/$package
+done
