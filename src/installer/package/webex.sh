22dbc5336ad6a1974ff64d17533e0ada41b24b0d
Bruno Martins Correa <brunomsft@gmail.com>
2021-10-31 23:44:14 -0300

    Adds webex

---
+++ src/installer/package/webex.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+case "$os_name" in
+    linux)
+        case "$distro_name" in
+            fedora)
+                official_packages+=(
+                    'https://binaries.webex.com/WebexDesktop-CentOS-Official-Package/Webex.rpm'
+                )
+                ;;
+        esac
+        ;;
+esac
