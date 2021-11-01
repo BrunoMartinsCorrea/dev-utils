93a12b4b4ef51d36ecd199a3652476fe64bbeb23
Bruno Martins Correa <brunomsft@gmail.com>
2021-10-31 22:28:22 -0300

    Adds gnome

--- src/script/fedora-based-utils.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo dnf install -y NetworkManager-ssh-gnome NetworkManager-openvpn-gnome dconf dconf-editor gnome-boxes gnome-tweak-tool sushi geary cheese eog gnome-builder gnome-calendar gnome-clocks gnome-contacts gnome-dictionary gnome-multi-writer gnome-photos gnome-remote-desktop gnome-sound-recorder gnome-logs gnome-todo gnome-weather peek pitivi epiphany gnome-latex gnome-books gnome-connections almanah seahorse gnome-system-monitor gnome-terminal tilix gnome-usage file-roller meld evince polari vinagre totem gnome-music nautilus chrome-gnome-shell gnome-shell-extension-gsconnect gnome-shell-extension-appindicator gnome-shell-extension-common gnome-shell-extension-dash-to-dock
