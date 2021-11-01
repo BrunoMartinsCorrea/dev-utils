90c1f28b316ff9e59549fd621fcf5833f8e6b9a6
Bruno Martins Correa <brunomsft@gmail.com>
2021-10-31 22:08:07 -0300

    Adds php

---
+++ src/installer/package/php.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+case "$os_name" in
+    linux)
+        case "$distro_name" in
+            fedora)
+                official_packages+=(
+                    'php'
+                )
+                ;;
+        esac
+        ;;
+esac
