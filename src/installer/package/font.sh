dc480604d87c79290387d08531d88976b100a18a
Bruno Martins Correa <brunomsft@gmail.com>
2021-10-31 22:09:36 -0300

    Adds font

---
+++ src/installer/package/font.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+case "$os_name" in
+    linux)
+        case "$distro_name" in
+            fedora)
+                official_packages+=(
+                    'jetbrains-mono-fonts-all'
+                    'powerline'
+                    'fira-code-fonts'
+                    'google-noto-emoji-color-fonts'
+                )
+                ;;
+        esac
+        ;;
+esac
