e78abec7b7405ec52b56d3ac8a211c81b97fcfc2
Bruno Martins Correa <bruno.correa@yssy.com.br>
2021-11-16 12:50:25 -0300

    Refactors vscode installation

--- src/installer/package/vscode.sh
+++ src/installer/package/vscode.sh
@@ -6,7 +6,7 @@ case "$os_name" in
             fedora)
                 pre_official_install_scripts+=(
                     'sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc'
-                    'sudo sh -c ''echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'''
+                    'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo'
                 )
                 official_packages+=(
                     'openssl-libs'
