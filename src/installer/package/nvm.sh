df5384deecf5f028bb85cace5b1fed6739b88bcf
Bruno Martins Correa <brunomsft@gmail.com>
2022-01-07 00:15:47 -0300

    Refactors github packages installation

--- src/installer/package/nvm.sh
+++ src/installer/package/nvm.sh
@@ -8,7 +8,8 @@ linux)
             'yarnpkg'
         )
         custom_install_scripts+=(
-            'curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash'
+            'NVM_LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r .tag_name)'
+            'curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_LATEST_VERSION/install.sh | bash'
             'source ~/.nvm/nvm.sh'
             'nvm install --lts'
             'yes | npm install -g nx @nestjs/cli react-native-cli create-react-app create-next-app vercel @angular/cli @vue/cli vue-native-cli json-server expo-cli netlify-cli tsdx'
--- src/installer/package/yq.sh
+++ src/installer/package/yq.sh
@@ -5,7 +5,8 @@ linux)
     case "$DISTRO_NAME" in
     fedora)
         custom_install_scripts+=(
-            'sudo wget https://github.com/mikefarah/yq/releases/download/v4.2.0/yq_linux_amd64 -O /usr/bin/yq'
+            'YQ_LATEST_VERSION=$(curl -s https://api.github.com/repos/mikefarah/yq/releases/latest | jq -r .tag_name)'
+            'sudo wget https://github.com/mikefarah/yq/releases/download/$YQ_LATEST_VERSION/yq_linux_amd64 -O /usr/bin/yq'
             'sudo chmod +x /usr/bin/yq'
         )
         ;;
