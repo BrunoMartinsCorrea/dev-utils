b1c2676e4f94cbac45cc4608f701ac9660743354
Bruno Martins Correa <brunomsft@gmail.com>
2022-01-07 00:43:47 -0300

    Refactors github packages installation

--- src/installer/package/ctop.sh
+++ src/installer/package/ctop.sh
@@ -5,7 +5,8 @@ linux)
     case "$DISTRO_NAME" in
     fedora)
         custom_install_scripts+=(
-            'sudo wget https://github.com/bcicen/ctop/releases/download/0.7.6/ctop-0.7.6-linux-amd64 -O /usr/local/bin/ctop'
+            'CTOP_LATEST_VERSION=$(curl -s https://api.github.com/repos/bcicen/ctop/releases/latest | jq -r .tag_name)'
+            'sudo wget https://github.com/bcicen/ctop/releases/download/$CTOP_LATEST_VERSION/ctop-$CTOP_LATEST_VERSION-linux-amd64 -O /usr/local/bin/ctop'
             'sudo chmod +x /usr/local/bin/ctop'
         )
         ;;
