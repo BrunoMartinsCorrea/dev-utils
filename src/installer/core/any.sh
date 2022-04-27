6cf7390751acc1b5c0f174050c679a3938cb33d6
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-04-27 13:23:03 -0300

    Adds ubuntu support for GDM conf file

--- src/installer/core/any.sh
+++ src/installer/core/any.sh
@@ -3,6 +3,10 @@
 PACKAGES_PATH=$INSTALLER_PATH/package
 CONFIG_PATH=$INSTALLER_PATH/config/any
 
+pre_official_install_scripts+=(
+    'sudo mkdir -p /etc/gdm/db/gdm.d'
+)
+
 for package in $(ls "$PACKAGES_PATH"); do
     source "$PACKAGES_PATH/$package"
 done
