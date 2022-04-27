f529901f938608313d11bd4758b3781c66128004
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-04-27 11:28:25 -0300

    Adds ubuntu support for clang, clisp and common tools

--- src/installer/package/common-tools.sh
+++ src/installer/package/common-tools.sh
@@ -5,20 +5,8 @@ linux)
     case "$DISTRO_NAME" in
     fedora)
         official_packages+=(
-            'cmake'
-            'tree'
             'vi'
-            'vim'
-            'nano'
-            'curl'
-            'xclip'
-            'git'
-            'zip'
-            'unzip'
-            'crypto-policies'
             'openssh'
-            'openvpn'
-            'ntfs-3g'
             'gtk3-devel'
             'gtk4-devel'
             'gstreamer1'
@@ -27,14 +15,47 @@ linux)
             'gstreamer1-plugins-good'
             'libaio'
             'libvirt'
-            'libvirt-daemon-config-network'
             'libvirt-daemon-kvm'
             'virt-install'
+            'qemu-kvm'
+        )
+        ;;&
+    ubuntu)
+        official_packages+=(
+            'vim-tiny'
+            'openssh-client'
+            'libgtk-3-dev'
+            'libgtk-4-dev'
+            'gstreamer1.0-tools'
+            'gstreamer1.0-libav'
+            'gstreamer1.0-plugins-base'
+            'gstreamer1.0-plugins-good'
+            'libaio1'
+            'libvirt-daemon'
+            'libvirt-daemon-system'
+            'virtinst'
+            'qemu-system'
+        )
+        ;;&
+    fedora | ubuntu)
+        official_packages+=(
+            'cmake'
+            'tree'
+            'vim'
+            'nano'
+            'curl'
+            'xclip'
+            'git'
+            'zip'
+            'unzip'
+            'crypto-policies'
+            'openvpn'
+            'ntfs-3g'
+            'libvirt-daemon-config-network'
             'virt-manager'
             'virt-viewer'
             'virt-top'
             'qemu'
-            'qemu-kvm'
         )
         post_official_install_scripts+=(
             'sudo systemctl start libvirtd'
