fd299fda0863e492a5394f62783b3b05a8f8d6ff
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-04-27 23:13:39 -0300

    Adds ubuntu support for Webex

--- src/installer/package/webex.sh
+++ src/installer/package/webex.sh
@@ -8,6 +8,13 @@ linux)
             'https://binaries.webex.com/WebexDesktop-CentOS-Official-Package/Webex.rpm'
         )
         ;;
+    ubuntu)
+        custom_install_scripts+=(
+            'wget -q https://binaries.webex.com/WebexDesktop-Ubuntu-Official-Package/Webex.deb'
+            'sudo apt install -y ./Webex.deb'
+            'rm -f ./Webex.deb'
+        )
+        ;;
     esac
     ;;
 esac
