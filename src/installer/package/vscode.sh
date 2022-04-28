bb11c56d80aec21cd16ad33560c4b692370603d0
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-04-27 22:43:54 -0300

    Adds ubuntu support for VSCode

--- src/installer/package/vscode.sh
+++ src/installer/package/vscode.sh
@@ -20,6 +20,18 @@ linux)
             'code'
         )
         ;;
+    ubuntu)
+        pre_official_install_scripts+=(
+            'wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg'
+            'sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/'
+            'sudo sh -c "echo \"deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main\" > /etc/apt/sources.list.d/vscode.list"'
+            'rm -f packages.microsoft.gpg'
+        )
+        official_packages+=(
+            'apt-transport-https'
+            'code'
+        )
+        ;;
     esac
     ;;
 esac
