095a94f5b3e51638949fd6c131f685434dc27b98
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-04-27 23:55:14 -0300

    Refactors ubuntu script

--- src/installer/core/ubuntu.sh
+++ src/installer/core/ubuntu.sh
@@ -3,7 +3,9 @@
 official_package_manager='sudo apt install -y'
 
 pre_official_install_scripts+=(
-    'curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -'
+    'wget -qO- https://apt.releases.hashicorp.com/gpg | gpg --dearmor > packages.hashicorp.gpg'
+    'sudo install -o root -g root -m 644 packages.hashicorp.gpg /etc/apt/trusted.gpg.d/'
+    'rm -f packages.hashicorp.gpg'
     'sudo apt-add-repository -y "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"'
     'sudo apt update -y'
     'sudo apt full-upgrade -y'
