7f11c73bcb3646087fb76b51d229cc83e323d751
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-04-27 22:29:26 -0300

    Adds ubuntu support for yarn

--- src/installer/package/nvm.sh
+++ src/installer/package/nvm.sh
@@ -3,19 +3,18 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
-    fedora)
+    fedora | ubuntu)
         official_packages+=(
             'yarnpkg'
+        )
+        custom_install_scripts+=(
+            'NVM_LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r .tag_name)'
+            'curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_LATEST_VERSION/install.sh | bash'
+            'source ~/.nvm/nvm.sh'
+            'nvm install --lts --default'
+            'yes | npm install -g nx @nestjs/cli react-native-cli create-react-app create-next-app vercel @angular/cli @vue/cli vue-native-cli json-server expo-cli netlify-cli tsdx'
         )
         ;;
     esac
     ;;
-
-    custom_install_scripts+=(
-        'NVM_LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r .tag_name)'
-        'curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_LATEST_VERSION/install.sh | bash'
-        'source ~/.nvm/nvm.sh'
-        'nvm install --lts --default'
-        'yes | npm install -g nx @nestjs/cli react-native-cli create-react-app create-next-app vercel @angular/cli @vue/cli vue-native-cli json-server expo-cli netlify-cli tsdx'
-    )
 esac
