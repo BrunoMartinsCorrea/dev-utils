1dd990bec310e70394d81dc643524202f8763702
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-04-27 22:45:07 -0300

    Adds ubuntu support for yq

--- src/installer/package/yq.sh
+++ src/installer/package/yq.sh
@@ -3,7 +3,7 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
-    fedora)
+    fedora | ubuntu)
         custom_install_scripts+=(
             'YQ_LATEST_VERSION=$(curl -s https://api.github.com/repos/mikefarah/yq/releases/latest | jq -r ".assets[].browser_download_url | select(. | test(\"linux_amd64\$\"))")'
             'sudo wget $YQ_LATEST_VERSION -O /usr/bin/yq'
