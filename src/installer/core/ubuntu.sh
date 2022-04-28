581f78a9e25d4805947d1b4f63f5e1c3f87cca51
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-04-27 22:48:46 -0300

    Adds ubuntu support for Hashicorp repository

--- src/installer/core/ubuntu.sh
+++ src/installer/core/ubuntu.sh
@@ -3,6 +3,8 @@
 official_package_manager='sudo apt install -y'
 
 pre_official_install_scripts+=(
+    'curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -'
+    'sudo apt-add-repository -y "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"'
     'sudo apt update -y'
     'sudo apt full-upgrade -y'
 )
