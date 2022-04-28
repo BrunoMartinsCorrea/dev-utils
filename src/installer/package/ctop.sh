c8822f6aa1e6d809ce30cff141c58806b629f25b
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-04-27 21:40:18 -0300

    Adds ubuntu support for ctop

--- src/installer/package/ctop.sh
+++ src/installer/package/ctop.sh
@@ -2,10 +2,19 @@
 
 case "$OS_NAME" in
 linux)
-    custom_install_scripts+=(
-        'CTOP_LATEST_VERSION=$(curl -s https://api.github.com/repos/bcicen/ctop/releases/latest | jq -r ".assets[].browser_download_url | select(. | test(\"linux-amd\"))")'
-        'sudo wget $CTOP_LATEST_VERSION -O /usr/local/bin/ctop'
-        'sudo chmod +x /usr/local/bin/ctop'
-    )
+    case "$DISTRO_NAME" in
+    ubuntu)
+        official_packages+=(
+            'ctop'
+        )
+        ;;
+    *)
+        custom_install_scripts+=(
+            'CTOP_LATEST_VERSION=$(curl -s https://api.github.com/repos/bcicen/ctop/releases/latest | jq -r ".assets[].browser_download_url | select(. | test(\"linux-amd\"))")'
+            'sudo wget $CTOP_LATEST_VERSION -O /usr/local/bin/ctop'
+            'sudo chmod +x /usr/local/bin/ctop'
+        )
+        ;;
+    esac
     ;;
 esac
