d60d5f9851d19ff3184c5f60a2c3868f0d15213b
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 17:46:09 -0300

    Adds arch support for ruby

--- src/installer/package/ruby.sh
+++ src/installer/package/ruby.sh
@@ -3,17 +3,17 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
-    fedora)
-        official_packages+=(
-            'rubygems'
-        )
-        ;;&
     ubuntu)
         official_packages+=(
             'rubygems-integration'
         )
         ;;&
-    fedora | ubuntu)
+    arch | fedora)
+        official_packages+=(
+            'rubygems'
+        )
+        ;;&
+    arch | fedora | ubuntu)
         official_packages+=(
             'ruby'
         )
