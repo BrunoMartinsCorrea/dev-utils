f96bc1c60c2222c00f9f6b62525412fe369ef8ba
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 17:19:06 -0300

    Adds arch support for kubernetes

--- src/installer/package/kubernetes.sh
+++ src/installer/package/kubernetes.sh
@@ -3,6 +3,14 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'helm'
+            'k9s'
+            'kubeadm'
+            'kubectl'
+        )
+        ;;
     fedora | ubuntu)
         official_packages+=(
             'kubernetes'
