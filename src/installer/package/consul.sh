fed85aee671605d914bbdfe7d07bd3482e3db94a
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 14:37:18 -0300

    Adds arch support for consul

--- src/installer/package/consul.sh
+++ src/installer/package/consul.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'consul-bin'
+        )
+        ;;
     fedora | ubuntu)
         official_packages+=(
             'consul'
