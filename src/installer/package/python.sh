35d0a14b5c396110e2abe0f72f343b1133bcc4af
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 17:41:38 -0300

    Adds arch support for python

--- src/installer/package/python.sh
+++ src/installer/package/python.sh
@@ -3,11 +3,19 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'python'
+            'python-pip'
+        )
+        ;;
     fedora | ubuntu)
         official_packages+=(
             'python3'
             'python3-pip'
         )
+        ;;
+    arch | fedora | ubuntu)
         post_official_packages+=(
             'pip install --user awscli localstack localstack-client jupyter pipenv virtualenv'
         )
