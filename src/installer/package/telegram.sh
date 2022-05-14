d4a67359fc9f62ac863bcd94bf6fecaddf8cd954
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 17:54:09 -0300

    Adds arch support for telegram

--- src/installer/package/telegram.sh
+++ src/installer/package/telegram.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'telegram-desktop'
+        )
+        ;;
     fedora | ubuntu)
         flatpak_packages+=(
             'org.telegram.desktop'
