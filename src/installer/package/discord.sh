7df280ee6a7e1bb2faac770117b817f7e8c9d803
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 14:48:02 -0300

    Adds arch support for discord

--- src/installer/package/discord.sh
+++ src/installer/package/discord.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'discord'
+        )
+        ;;
     fedora | ubuntu)
         flatpak_packages+=(
             'com.discordapp.Discord'
