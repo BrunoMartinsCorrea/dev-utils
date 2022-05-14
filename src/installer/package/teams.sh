0a40779ba40f3f47e01419a94447d1949eab015c
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 17:52:51 -0300

    Adds arch support for teams

--- src/installer/package/teams.sh
+++ src/installer/package/teams.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'teams'
+        )
+        ;;
     fedora | ubuntu)
         flatpak_packages+=(
             'com.microsoft.Teams'
