1cf5bbf9b6fac7f34128b90b60d6f4f5f429704f
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 17:00:51 -0300

    Adds arch support for insomnia

--- src/installer/package/insomnia.sh
+++ src/installer/package/insomnia.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'insomnia-bin'
+        )
+        ;;
     fedora | ubuntu)
         flatpak_packages+=(
             'rest.insomnia.Insomnia'
