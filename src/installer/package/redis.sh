1edb9e8c71c73ba6fd5f7545b028726adb047b27
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 17:44:56 -0300

    Adds arch support for redis

--- src/installer/package/redis.sh
+++ src/installer/package/redis.sh
@@ -3,6 +3,12 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'redis'
+            'resp-app'
+        )
+        ;;
     fedora | ubuntu)
         flatpak_packages+=(
             'app.resp.RESP'
