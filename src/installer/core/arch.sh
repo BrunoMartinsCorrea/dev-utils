7d8352e01eedf5f9d8013b953842f8490150233f
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 18:14:45 -0300

    Refactors arch support in installer script

--- src/installer/core/arch.sh
+++ src/installer/core/arch.sh
@@ -3,7 +3,7 @@
 official_package_manager='paru -Syuq --needed --sudoloop --noconfirm --noprogressbar'
 
 pre_official_install_scripts+=(
-    'sudo pacman -Syu --noconfirm pacman-contrib base-devel posix-user-portability cmake git rustup'
+    'sudo pacman -Syuq --needed --noconfirm --noprogressbar pacman-contrib base-devel posix-user-portability cmake git rustup'
     'curl -s https://archlinux.org/mirrorlist/\?country\=BR | sed "s/^#Server/Server/" | sudo tee /etc/pacman.d/mirrorlist'
     'rankmirrors /etc/pacman.d/mirrorlist'
     'rustup install stable'
