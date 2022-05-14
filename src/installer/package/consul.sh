8b9bf6c0f181aa28d96252e85f25477c302e2b97
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 14:44:00 -0300

    Refactors arch support for consul

--- src/installer/package/consul.sh
+++ src/installer/package/consul.sh
@@ -3,12 +3,7 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
-    arch)
-        official_packages+=(
-            'consul-bin'
-        )
-        ;;
-    fedora | ubuntu)
+    arch | fedora | ubuntu)
         official_packages+=(
             'consul'
         )
