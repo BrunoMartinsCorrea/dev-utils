4522ad549043a965aef5751e1319beed2dd1c75c
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 17:40:08 -0300

    Adds arch support for postman

--- src/installer/package/postman.sh
+++ src/installer/package/postman.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'postman-bin'
+        )
+        ;;
     fedora | ubuntu)
         flatpak_packages+=(
             'com.getpostman.Postman'
