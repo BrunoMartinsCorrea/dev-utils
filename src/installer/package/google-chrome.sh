7117ef567b063faf8eab06625475977648131ae0
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 16:50:52 -0300

    Adds arch support for google-chrome

--- src/installer/package/google-chrome.sh
+++ src/installer/package/google-chrome.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'google-chrome'
+        )
+        ;;
     fedora | ubuntu)
         flatpak_packages+=(
             'com.google.Chrome'
