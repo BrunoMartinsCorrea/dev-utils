6b51521f399efef4a3b7f051e0398479953320d1
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 18:07:22 -0300

    Adds arch support for yq

--- src/installer/package/yq.sh
+++ src/installer/package/yq.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'yq'
+        )
+        ;;
     fedora | ubuntu)
         custom_install_scripts+=(
             'YQ_LATEST_VERSION=$(curl -s https://api.github.com/repos/mikefarah/yq/releases/latest | jq -r ".assets[].browser_download_url | select(. | test(\"linux_amd64\$\"))")'
