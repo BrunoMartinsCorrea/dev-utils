b27ebc144fbcb341978b03a51985657d99cb900a
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 16:49:31 -0300

    Adds arch support for golang

--- src/installer/package/golang.sh
+++ src/installer/package/golang.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'go'
+        )
+        ;;
     fedora | ubuntu)
         official_packages+=(
             'golang'
