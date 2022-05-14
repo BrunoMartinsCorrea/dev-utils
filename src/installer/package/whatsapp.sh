4d6cdf3fc22dc378cc37e3db52d79232f4323a58
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 18:06:49 -0300

    Adds arch support for whatsapp

--- src/installer/package/whatsapp.sh
+++ src/installer/package/whatsapp.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'whatsapp-for-linux'
+        )
+        ;;
     fedora | ubuntu)
         flatpak_packages+=(
             'com.rtosta.zapzap'
