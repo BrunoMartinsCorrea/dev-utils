f503305dce11315a35fb942a9f78742b8c01a6f6
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 14:54:42 -0300

    Adds arch support for figma

--- src/installer/package/figma.sh
+++ src/installer/package/figma.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'figma-linux'
+        )
+        ;;
     fedora | ubuntu)
         flatpak_packages+=(
             'io.github.Figma_Linux.figma_linux'
