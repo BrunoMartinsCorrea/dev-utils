7c969196734efb595609da8f0488201cc3315f54
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 16:54:28 -0300

    Adds arch support for haskell

--- src/installer/package/haskell.sh
+++ src/installer/package/haskell.sh
@@ -3,6 +3,13 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'cabal-install'
+            'ghc'
+            'stack'
+        )
+        ;;
     fedora | ubuntu)
         official_packages+=(
             'haskell-platform'
