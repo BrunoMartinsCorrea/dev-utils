4261883dc290aeda5942bc86d5ff365c53e22661
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-14 14:46:20 -0300

    Adds arch support for dbeaver

--- src/installer/package/dbeaver.sh
+++ src/installer/package/dbeaver.sh
@@ -3,6 +3,11 @@
 case "$OS_NAME" in
 linux)
     case "$DISTRO_NAME" in
+    arch)
+        official_packages+=(
+            'dbeaver'
+        )
+        ;;
     fedora | ubuntu)
         flatpak_packages+=(
             'io.dbeaver.DBeaverCommunity'
