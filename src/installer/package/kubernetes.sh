7c70cf31ee6c0c9a022f2d64ef916edd82e95240
Bruno Martins Correa <bruno.correa@kamino.com.br>
2022-05-21 15:08:22 -0300

    Adds minikube

--- src/installer/package/kubernetes.sh
+++ src/installer/package/kubernetes.sh
@@ -5,12 +5,28 @@ linux)
     case "$DISTRO_NAME" in
     arch)
         official_packages+=(
-            'helm'
             'k9s'
             'kubeadm'
             'kubectl'
         )
-        ;;
+        ;;&
+    fedora)
+        official_packages+=(
+            'https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm'
+        )
+        ;;&
+    ubuntu)
+        custom_install_scripts+=(
+            'wget -q --show-progress https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb -O minikube.deb'
+            'sudo apt install -y ./minikube.deb'
+            'rm -f ./minikube.deb'
+        )
+        ;;&
+    arch | fedora)
+        official_packages+=(
+            'helm'
+        )
+        ;;&
     fedora | ubuntu)
         official_packages+=(
             'kubernetes'
