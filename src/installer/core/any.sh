bb58dd902cd0e5c0dc84e10d0bd1c01d63e267f5
Bruno Martins Correa <brunomsft@gmail.com>
2022-06-15 23:28:41 -0300

    Refactors rc scripts

--- src/installer/core/any.sh
+++ src/installer/core/any.sh
@@ -3,6 +3,10 @@
 PACKAGES_PATH=$INSTALLER_PATH/package
 CONFIG_PATH=$INSTALLER_PATH/config/any
 
+environment_variables+=(
+    'export PATH=$PATH:$HOME/.local/bin'
+)
+
 for package in $(ls "$PACKAGES_PATH"); do
     source "$PACKAGES_PATH/$package"
 done
